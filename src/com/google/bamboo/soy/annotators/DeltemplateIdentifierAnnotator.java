5365048c8deaa863447c102c2744c36295d9376c
Svyatoslav Usachev <susachev@google.com>
2017-07-10 14:34:58 +0100

    Annotating bad names of delegate templates. (#39)
    * Annotating bad names of delegate templates.
    
    * Adding forgotten file.

---
+++ src/com/google/bamboo/soy/annotators/DeltemplateIdentifierAnnotator.java
@@ -0,0 +1,35 @@
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.bamboo.soy.annotators;
+
+import com.google.bamboo.soy.parser.SoyTemplateBlock;
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+public class DeltemplateIdentifierAnnotator implements Annotator {
+  @Override
+  public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder) {
+    if (psiElement instanceof SoyTemplateBlock) {
+      SoyTemplateBlock block = (SoyTemplateBlock) psiElement;
+      if (block.isDelegate() && block.getName() != null && block.getName().startsWith(".")) {
+        annotationHolder.createErrorAnnotation(
+            // cannot be null since getName is not null
+            block.getDefinitionIdentifier(), "Delegate template names cannot start with '.'.");
+      }
+    }
+  }
+}
