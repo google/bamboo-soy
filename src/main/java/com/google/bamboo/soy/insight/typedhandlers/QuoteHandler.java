d9aa81afb47f1db7ad99b4142c5f751aac382ea4
Thibault Sottiaux <thso@google.com>
2017-07-30 21:49:36 +0100

    Improve insertion of matching characters

--- src/main/java/com/google/bamboo/soy/insight/typedhandlers/QuoteHandler.java
+++ src/main/java/com/google/bamboo/soy/insight/typedhandlers/QuoteHandler.java
@@ -19,14 +19,27 @@ import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiDocumentManager;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
 
-/** Inserts a matching single or double quote when typing one. */
+/**
+ * Inserts a matching closing character (quote, parenthesis, bracket) when typing one.
+ */
 public class QuoteHandler implements TypedActionHandler {
-  private final Set<Character> matchingQuotes = ImmutableSet.of('"', '\'');
+
+  private Character lastInsertedChar;
+  private int lastInsertedOffset;
+
+  private final Set<Pair<Character, Character>> matchingCharacters = ImmutableSet
+      .of(Pair.create('"', '"'), Pair.create('\'', '\''), Pair.create('(', ')'),
+          Pair.create('[', ']'));
+  private final Set<Character> alwaysCloseCharacters = ImmutableSet.of('(', '[');
+  private final Set<String> allowedPreviousCharacters = ImmutableSet
+      .of("\n", " ", "[", "]", "(", ")", "=");
+  private final Set<String> allowedNextCharacters = ImmutableSet.of("\n", " ", "]", ")", "}");
   private final TypedActionHandler myOriginalHandler;
 
   public QuoteHandler(TypedActionHandler originalHandler) {
@@ -34,17 +47,31 @@ public class QuoteHandler implements TypedActionHandler {
   }
 
   public void execute(@NotNull Editor editor, char charTyped, @NotNull DataContext dataContext) {
-    if (matchingQuotes.contains(charTyped)) {
-      Document document = editor.getDocument();
-      int caretOffset = editor.getCaretModel().getOffset();
+    Document document = editor.getDocument();
+    int caretOffset = editor.getCaretModel().getOffset();
 
-      String prevChar = getPreviousChar(document, caretOffset);
-      String nextChar = getNextChar(document, caretOffset);
+    String prevChar = getPreviousChar(document, caretOffset);
+    String nextChar = getNextChar(document, caretOffset);
 
-      if (prevChar.equals(" ") && !nextChar.equals(charTyped + "")) {
-        document.insertString(editor.getCaretModel().getOffset(), charTyped + "");
-        if (editor.getProject() != null) {
-          PsiDocumentManager.getInstance(editor.getProject()).commitDocument(document);
+    // Prevent from re-typing the same character that was just automatically inserted.
+    if (caretOffset == lastInsertedOffset && lastInsertedChar.equals(charTyped) && nextChar
+        .equals(lastInsertedChar + "")) {
+      editor.getCaretModel().moveToOffset(caretOffset + 1);
+      return;
+    }
+
+    for (Pair<Character, Character> charPair : matchingCharacters) {
+      if (charPair.first.equals(charTyped)) {
+        if ((alwaysCloseCharacters.contains(charPair.first) || allowedPreviousCharacters
+            .contains(prevChar)) && allowedNextCharacters.contains(nextChar)
+            && !nextChar.equals(charPair.second + "")) {
+          document.insertString(editor.getCaretModel().getOffset(), charPair.second + "");
+          lastInsertedChar = charPair.second;
+          lastInsertedOffset = caretOffset + 1;
+
+          if (editor.getProject() != null) {
+            PsiDocumentManager.getInstance(editor.getProject()).commitDocument(document);
+          }
         }
       }
     }
