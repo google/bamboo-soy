5d30944101879859025374bf1a9a50ce96470370
Alexander Pavlov <apavlov@google.com>
2020-01-23 00:24:04 +0100

    Support list comprehensions

--- src/test/java/com/google/bamboo/soy/parser/SoyParserTest.java
+++ src/test/java/com/google/bamboo/soy/parser/SoyParserTest.java
@@ -93,6 +93,10 @@ public class SoyParserTest extends ParsingTestCase {
     doTest(true);
   }
 
+  public void testLists() {
+    doTest(true);
+  }
+
   @Override
   protected boolean checkAllPsiRoots() {
     return false;
